213c21a2d26d70f27b74c281282826e40979aefb
Chancel.Yang <ycs1026@vip.qq.com>
2023-11-30 09:32:22 +0000

    Add goreleaser Replace webp with png

--- go.mod
+++ go.mod
@@ -3,11 +3,8 @@ module go-tools-api/m
 go 1.15
 
 require (
-	github.com/chai2010/webp v1.1.1
 	github.com/gin-gonic/gin v1.8.1
-	github.com/go-ini/ini v1.66.6
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
-	github.com/labstack/gommon v0.3.1
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
--- utils/image.go
+++ utils/image.go
@@ -1,23 +1,22 @@
 package utils
 
 import (
-	"bytes"
-	"io"
+	"image/png"
 	"os"
 
-	"github.com/chai2010/webp"
 	"github.com/nfnt/resize"
 )
 
 func ClipImageWithTmpFile(sourcePath string, width, height int) (string, error) {
 	// 读取源文件
-	srcFile, err := os.ReadFile(sourcePath)
+	srcFile, err := os.Open(sourcePath)
 	if err != nil {
 		return "", err
 	}
+	defer srcFile.Close()
 
-	// 解码 WebP 图像
-	srcImg, err := webp.DecodeRGB(srcFile)
+	// 解码 PNG 图像
+	srcImg, err := png.Decode(srcFile)
 	if err != nil {
 		return "", err
 	}
@@ -26,21 +25,15 @@ func ClipImageWithTmpFile(sourcePath string, width, height int) (string, error) 
 	dstImg := resize.Resize(uint(width), uint(height), srcImg, resize.Lanczos3)
 
 	// 创建临时文件
-	dstFile, err := os.CreateTemp("", "image-*.webp")
+	dstFile, err := os.CreateTemp("", "image-*.png")
 	if err != nil {
 		return "", err
 	}
+	defer dstFile.Close()
 
-	// 将裁剪后的图像编码为 WebP 格式，然后写入临时文件
-	dstData, err := webp.EncodeRGB(dstImg, 90)
+	// 将裁剪后的图像编码为 PNG 格式，然后写入临时文件
+	err = png.Encode(dstFile, dstImg)
 	if err != nil {
-		dstFile.Close()
-		os.Remove(dstFile.Name())
-		return "", err
-	}
-	_, err = io.Copy(dstFile, bytes.NewReader(dstData))
-	if err != nil {
-		dstFile.Close()
 		os.Remove(dstFile.Name())
 		return "", err
 	}
